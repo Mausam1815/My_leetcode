5decd9cdf7ad7d0572c5ac1a88bc9f0e0402d39b
Mausam Raj <128462297+Mausam1815@users.noreply.github.com>
2023-08-13 20:59:07 +0530

    added solution

---
+++ 1657. Determine if Two Strings Are Close/Main.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        // Calculate character frequencies for both strings
+        for (char c : word1.toCharArray()) {
+            freq1[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            freq2[c - 'a']++;
+        }
+
+        // Check if the character frequencies match
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        // Sort the frequency arrays to compare
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        // Check if the sorted frequency arrays match
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
